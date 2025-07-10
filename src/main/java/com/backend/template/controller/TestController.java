9c49b46fab71a934c762e089fc8badf4b229d26d
정지원 <jeongjiwon@jeongjiwon-ui-MacBookPro.local>
2025-07-10 16:53:10 +0900

    feature: health 컨트럴러 생성

--- src/main/java/com/backend/template/controller/TestController.java
+++ src/main/java/com/backend/template/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String getTestMessage() {
         return "healthy server!";
     }
