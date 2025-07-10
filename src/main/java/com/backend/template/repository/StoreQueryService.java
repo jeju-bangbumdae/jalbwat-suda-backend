efd25ef85b44ae7b8b02120d5415193142cb9805
정지원 <jeongjiwon@jeongjiwon-ui-MacBookPro.local>
2025-07-10 22:51:23 +0900

    chore: cors 추가

--- src/main/java/com/backend/template/repository/StoreQueryService.java
+++ src/main/java/com/backend/template/repository/StoreQueryService.java
@@ -1,8 +1,12 @@
 package com.backend.template.repository;
 
 import com.backend.template.dto.StoreResponseDto;
+import com.backend.template.entity.QStore;
+import com.backend.template.entity.QUser;
+import com.backend.template.entity.Store;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class StoreQueryService {
     private final JPAQueryFactory qf;
+
+    public Store findBy(Point point) {
+        QStore store = QStore.store;
+        QUser user = QUser.user;
+
+    }
 }
 
