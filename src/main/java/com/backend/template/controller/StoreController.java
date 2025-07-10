f104966473150c6db785106dfad4983db3490fff
정지원 <jeongjiwon@jeongjiwon-ui-MacBookPro.local>
2025-07-10 19:48:17 +0900

    feature: cors

--- src/main/java/com/backend/template/controller/StoreController.java
+++ src/main/java/com/backend/template/controller/StoreController.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController()
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Tag(name = "Store API", description = "상점 관련 API")
 public class StoreController {
--- src/main/java/com/backend/template/controller/UserController.java
+++ src/main/java/com/backend/template/controller/UserController.java
@@ -13,9 +13,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController()
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Tag(name = "User API", description = "사용자 관련 API")
 public class UserController {
